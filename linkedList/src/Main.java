8d877e8ce50d11c11cfdee2f798c0313eb40d63e
teniola763 <115806622+teniola763@users.noreply.github.com>
2024-08-02 15:18:39 +0100

    Add files via upload

---
+++ linkedList/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> object = new LinkedList<String>();
+        object.add("book");
+        object.add("car");
+        object.add("bottle");
+        object.add("table");
+        System.out.println(object);
+
+    }
+}
